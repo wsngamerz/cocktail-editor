b638a798eb32f02715d14ef06ae69b48892037a3
William Neild <wneild5@gmail.com>
2023-10-20 14:39:15 +0100

    fix: no longer fetch from self, use internal function

--- src/app/cocktail/page.tsx
+++ src/app/cocktail/page.tsx
@@ -1,12 +1,9 @@
 import CocktailView from "@/components/cocktail/View";
-import fetcher, { cocktailAsDisplayCocktail, getCocktail } from "@/lib/utils";
-import { IngredientResponse } from "@/hooks/useIngredients";
+import { cocktailAsDisplayCocktail, getCocktail, getIngredients } from "@/lib/utils";
 
 export default async function CocktailPage() {
-  const ingredientsResponse = await fetcher<IngredientResponse>(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/api/ingredients`);
-
   const cocktail = getCocktail();
-  const displayCocktail = cocktailAsDisplayCocktail(cocktail, ingredientsResponse.data);
+  const displayCocktail = cocktailAsDisplayCocktail(cocktail, getIngredients()?.data);
 
   return (
     <div className="max-w-6xl mx-auto px-4 py-8">
